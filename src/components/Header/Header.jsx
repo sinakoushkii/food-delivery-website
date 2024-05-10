b0b3e4cb3e4e64a76f223bec59e1815b370a9940
sina <sinaderik7@gmail.com>
2024-05-10 22:40:59 +0430

    feat: responsive menu created

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import logo from '../../assets/images/res-logo.png'
 import { Container } from 'reactstrap'
 import { NavLink, Link } from 'react-router-dom'
+import '../../styles/header.css'
 
 export default function Header() {
 
@@ -23,22 +24,34 @@ export default function Header() {
       path: "/contact"
     },
   ]
+
+  const menuRef = useRef(null)
+  const toggleMenu = () => menuRef.current.classList.toggle('show__menu')
+
+
   return (
     <header className="header">
       <Container>
-        <div className="nav__wrapper">
+        <div className="nav__wrapper d-flex align-items-center justify-content-between">
           <div className="logo">
             <img src={logo} alt="logo" />
             <h5>Tasty Treat</h5>
           </div>
-          
+
           {/* ======= menu ======= */}
-          
-          <div className="navigation">
-            <div className="menu">
+
+          <div className="navigation" ref={menuRef} onClick={toggleMenu}>
+            <div className="menu d-flex align-items-center gap-5">
               {
                 nav__link.map((item, index) => (
-                  <NavLink key={index} to={item.path}>{item.display}</NavLink>
+                  <NavLink
+
+                    className={navClass => navClass.isActive ? "active__menu" : ""}
+                    key={index}
+                    to={item.path}
+                  >
+                    {item.display}
+                  </NavLink>
                 ))
               }
             </div>
@@ -46,13 +59,19 @@ export default function Header() {
 
           {/* ======= nav right icons ======= */}
 
-          <div className="nav__right">
+          <div className="nav__right d-flex align-items-center gap-4">
             <span className="cart__icon">
-            <i className="ri-shopping-cart-line"></i>
+              <i className="ri-shopping-cart-line"></i>
               <span className='cart__badge'>2</span>
             </span>
+            <span className='user'>
+              <Link to='/login'><i className="ri-user-fill"></i></Link>
+            </span>
+            <span className='mobile__menu' onClick={toggleMenu}>
+              <i className="ri-menu-line"></i>
+            </span>
           </div>
-          
+
         </div>
       </Container>
     </header>
