1bac24465348c9e07cca25c45545f791d78ad671
sina <sinaderik7@gmail.com>
2024-08-20 18:54:45 +0430

    feat: addToCart button now is actived

--- src/components/Ui/productCart/ProductCart.jsx
+++ src/components/Ui/productCart/ProductCart.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import '../../../styles/productCard.css'
+import { useDispatch } from 'react-redux'
+import { cartActions } from '../../../store/shopping-cart/cartSlice'
 
 
 export default function ProductCart({ item }) {
     const { id, title, image01, price } = item
-    
+    const dispatch=useDispatch()
+   
+    const addToCart=()=>{
+        dispatch(cartActions.addItem({
+            id,
+            title,
+            image01,
+            price
+        }))
+    }
+
     return (
         <div className='product__item'>
             <div className='product__img'>
@@ -15,7 +27,7 @@ export default function ProductCart({ item }) {
                 <h5><Link to={`/foods/${id}`}>{title}</Link></h5>
                 <div className='product__btn-wrapper'>
                     <span className='product__price'>${price}</span>
-                    <button className='addtoCart__btn'>
+                    <button className='addtoCart__btn' onClick={addToCart}>
                         <span>Add to Cart</span>
                     </button>
                 </div>
