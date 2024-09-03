249d26de8c60bf2cac15f7054d617fb6992862fc
Sina <sinaderik7@gmail.com>
2024-09-03 23:59:24 +0330

    feart: cart page created

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,69 @@
 import React from 'react'
+import Helemt from '../components/Helmet/Helmet'
+import CommonSection from '../components/Ui/common-section/CommonSection'
+import { Container, Row, Col, Toast } from 'reactstrap'
+import '../styles/cart-page.css'
+import { useDispatch, useSelector } from 'react-redux'
+import {cartActions} from '../store/shopping-cart/cartSlice'
+
+const Tr = props => {
+  const { id, image01, title, price, quantity } = props.item
+  const dispatch = useDispatch()
+
+  const deleteItem = () => {
+    dispatch(cartActions.deleteItem(id))
+  }
+  return (
+    <tr>
+      <td className='cart__img-box'>
+        <img src={image01} alt="product-image" />
+      </td>
+      <td>{title}</td>
+      <td>${price}</td>
+      <td>{quantity}x</td>
+      <td className='cart__item-del'>
+        <i onClick={()=>deleteItem()} className="ri-delete-bin-line"></i>
+      </td>
+    </tr>
+  )
+}
 
 export default function Cart() {
+  const cartItems = useSelector(state => state.cart.cartItems)
+  const totalAmount = useSelector(state => state.cart.totalAmount)
+
   return (
-    <div>Cart</div>
+    <Helemt title='cart'>
+      <CommonSection title='your cart' />
+      <section>
+        <Container>
+          <Row>
+            <Col lg='12'>
+              {cartItems.length === 0 ?
+                <h5 className='text-center'>Cart is empty</h5> :
+                <table className='table table-bordered'>
+                  <thead>
+                    <tr>
+                      <th>Image</th>
+                      <th>Product   Title</th>
+                      <th>Price</th>
+                      <th>Quantity</th>
+                      <th>Delete</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {cartItems.map(item => <Tr item={item} key={item.id} />)}
+                  </tbody>
+                </table>
+              }
+              <div>
+                <h6>Total: <span>{totalAmount}</span></h6>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helemt>
   )
+
 }
